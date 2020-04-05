b39ac5bf86b6649bcd236fa038d4d3be1b91d595
gaolei <gaol@spsing.com>
2020-04-05 14:57:30 +0800

    编写视频类型布局文件

--- app/src/main/java/com/vinsuan/ppjoke/view/PPImageView.java
+++ app/src/main/java/com/vinsuan/ppjoke/view/PPImageView.java
@@ -5,12 +5,19 @@ import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.widget.AppCompatImageView;
 import androidx.databinding.BindingAdapter;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.RequestBuilder;
 import com.bumptech.glide.load.resource.bitmap.CircleCrop;
+import com.bumptech.glide.request.target.SimpleTarget;
+import com.bumptech.glide.request.transition.Transition;
+import com.vinsuan.libcommon.PixUtils;
+
+import jp.wasabeef.glide.transformations.BlurTransformation;
 
 public class PPImageView extends AppCompatImageView {
     public PPImageView(Context context) {
@@ -26,16 +33,62 @@ public class PPImageView extends AppCompatImageView {
     }
 
 
-    @BindingAdapter(value = {"image_url","isCircle"},requireAll = false)
-    public static void setImageUrl(PPImageView view,String imageUrl,boolean isCircle){
+    @BindingAdapter(value = {"image_url", "isCircle"}, requireAll = false)
+    public static void setImageUrl(PPImageView view, String imageUrl, boolean isCircle) {
         RequestBuilder<Drawable> builder = Glide.with(view).load(imageUrl);
-        if(isCircle){
+        if (isCircle) {
             builder.transform(new CircleCrop());
         }
         ViewGroup.LayoutParams layoutParams = view.getLayoutParams();
-        if(layoutParams!=null&&layoutParams.width>0&&layoutParams.height>0){
-            builder.override(layoutParams.width,layoutParams.height);
+        if (layoutParams != null && layoutParams.width > 0 && layoutParams.height > 0) {
+            builder.override(layoutParams.width, layoutParams.height);
         }
         builder.into(view);
+    }
+    public void bindData(int widthPx, int heightPx, int marginLieft, String imgUrl){
+        bindData(widthPx,heightPx,marginLieft,PixUtils.getScreenWidth(),PixUtils.getScreenHeight(),imgUrl);
+    }
+    public void bindData(int widthPx, int heightPx, int marginLieft, int maxWidth, int maxHeight, String imgUrl) {
+        if (widthPx <= 0 || heightPx <= 0) {
+            Glide.with(this).load(imgUrl).into(new SimpleTarget<Drawable>() {
+                @Override
+                public void onResourceReady(@NonNull Drawable resource, @Nullable Transition<? super Drawable> transition) {
+                    int height = resource.getIntrinsicHeight();
+                    int width = resource.getIntrinsicWidth();
+                    setSize(width, height, marginLieft, maxWidth, maxHeight);
+
+                    setImageDrawable(resource);
+                }
+            });
+            return;
+        }
+        setSize(widthPx,heightPx,marginLieft,maxWidth,maxHeight);
+        setImageUrl(this,imgUrl,false);
+    }
+
+    private void setSize(int width, int height, int marginLieft, int maxWidth, int maxHeight) {
+        int finalWidth, finalHeight;
+        if (width > height) {
+            finalWidth = maxWidth;
+            finalHeight = (int) (height / (width * 1.0f / finalWidth));
+        } else {
+            finalHeight = maxHeight;
+            finalWidth = (int) (width / (height * 1.0f / finalHeight));
+        }
+
+        ViewGroup.MarginLayoutParams params = new ViewGroup.MarginLayoutParams(finalWidth, finalHeight);
+        params.leftMargin = height > width ? PixUtils.dp2px(marginLieft) : 0;
+        setLayoutParams(params);
+
+    }
+
+    public void setBlurImageUrl(String coverUrl, int radius) {
+        Glide.with(this).load(coverUrl).override(50).transform(new BlurTransformation()).dontAnimate()
+                .into(new SimpleTarget<Drawable>() {
+                    @Override
+                    public void onResourceReady(@NonNull Drawable resource, @Nullable Transition<? super Drawable> transition) {
+                        setBackground(resource);
+                    }
+                });
     }
 }
